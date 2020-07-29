5d4082d4de7a2b9455182e79b351c8a38f1d9b81
Akhilchowdary97 <67588260+Akhilchowdary97@users.noreply.github.com>
2020-07-29 19:59:08 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,6 @@ from myapp import views
 app_name="myapp"
 urlpatterns = [
     path('',views.index,name='index'),
+    path('child',views.child,name='child'),
+    path('home',views.home,name="home"),
 ]
